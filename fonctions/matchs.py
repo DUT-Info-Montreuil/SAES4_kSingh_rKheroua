a353b14311a4df5b5fa809dda6b90a3cf90f8c88
KEVINSINGH261 <kevin.singh261@gmail.com>
2024-04-05 01:42:44 +0200

    correction methode ajout match poule

--- fonctions/matchs.py
+++ fonctions/matchs.py
@@ -16,26 +16,25 @@ def creer_match(arbitre, participant1_id, participant2_id):
     return match_id
    
 
+import itertools
+
+import itertools
+
 def creer_matchs_poule(id_poule):
     poule = poules.find_one({"_id": ObjectId(id_poule)})
-    equipes_poule = poule.get("equipes", [])  # Récupérer les équipes de la poule
-
-    # Créer des combinaisons de quatre équipes
+    equipes_poule = poule.get("equipes", []) 
     combinaisons_equipes = list(itertools.combinations(equipes_poule, 4))
 
-    # Créer des matchs pour chaque combinaison d'équipes
     for combinaison in combinaisons_equipes:
-        # Créer tous les matchs possibles entre les équipes de cette combinaison
         for i in range(len(combinaison)):
             for j in range(i + 1, len(combinaison)):
                 equipe1_id = combinaison[i]
                 equipe2_id = combinaison[j]
 
-                # Créer le match entre les deux équipes
-                
-                m = creer_match(arbitre="Arbitre1", participant1_id=equipe1_id, participant2_id=equipe2_id)
-                poules.update_one({"_id": poule}, {"$addToSet": { "matches":m}})
-                tournois.update_one({ "_id":poule["tournoi"]}, {"$addToSet": { "matches":m}})
+                match_id = creer_match(arbitre="", participant1_id=equipe1_id, participant2_id=equipe2_id)
+
+                poules.update_one({"_id": ObjectId(id_poule)}, {"$addToSet": {"matches": match_id}})
+
 
 def trouver_match_par_id(_id):
     match = matchs.find_one({"_id": ObjectId(_id)})
